16b8d2e5278fa843544206a289f09b2fc89d4707
Roman <roman@appacademy.io>
2013-04-22 10:51:47 -0700

    Add tasks/list template

--- app/assets/javascripts/views/tasks_list_view.js
+++ app/assets/javascripts/views/tasks_list_view.js
@@ -1,15 +1,12 @@
-TD.Views.TasksListView = Backbone.extend({
+TD.Views.TasksListView = Backbone.View.extend({
   render: function () {
     var that = this;
 
-    var $ul = $("<ul></ul>");
-    _(that.collection).each(function (task) {
-      $ul.append(
-        $("<li></li>").text(task.title)
-      );
+    var renderedContent = JST["tasks/list"]({
+      tasks: that.collection
     });
 
-    that.$el.html($ul);
+    that.$el.html(renderedContent);
     return that;
   }
 });
